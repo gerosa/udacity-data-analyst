6170129538fb9ad8a32aa7e40f19c472bbde684a
Luiz Gerosa <gerosa@gmail.com>
2017-11-15 17:55:18 -0200

    First version

---
+++ P6/README.md
@@ -0,0 +1,17 @@
+## P6: Make Effective Data Visualization
+*Create a data visualization from a data set that tells a story or highlights trends or patterns in the data. Use either dimple.js or d3.js to create the visualization. Your work should be a reflection of the theory and practice of data visualization, such as visual encodings, design principles, and effective communication.*
+
+### Summary
+When Brazilian consumers need to resolve a dispute with business the first step is to go to a local Procon (Consumer Protection Agency) and file a complaint. The Procon assists the consumer and intermediates the resolution with the company. The goal of this visulation is to compare how the Procon in different Brazilian states peform.
+
+### Design
+A scatter plot was chosen as the final chart type because it allows the comparasion between the resolution rate and the median time to resolve issues for each state. The size of the bubble represents the number of complaints filed in each state. The vertical line represents the weighted mean resolution rate and the horizontal line represents the weighted mean resolution time. 
+
+### Feedback
+
+### Resources
+
+- [Brazilian Business Complaints](https://www.kaggle.com/gerosa/procon)
+- [2015 Brazilian Business Complaints EDA](https://www.kaggle.com/gerosa/brazilian-consumer-2015-complaints-eda)
+- [Dimple](http://dimplejs.org/)
+
---
+++ P6/data/etl.R
@@ -0,0 +1,58 @@
+
+library(dplyr)
+
+Sys.setlocale(category = "LC_ALL", locale = "pt_BR.UTF-8")
+ds = read.csv("reclamacoes-fundamentadas-sindec-2015.csv", 
+              sep=',', encoding='utf-8', stringsAsFactors = FALSE, strip.white = TRUE)
+
+# keeping only columns used in this analysis
+ds <- subset(ds, select=c(AnoCalendario, DataArquivamento, DataAbertura, UF,
+                          Atendida))
+
+# renaming columns
+names(ds) <- c('year', 'closingDate', 'registerDate', 'state', 'resolved')
+
+# replacing empty cells with NA
+ds[ds == ''] <- NA
+ds[ds == 'NULL'] <- NA
+
+# removing rows with any missing value (NA)
+ds <- na.omit(ds)
+
+# fixing data types
+ds$closingDate <- as.Date(ds$closingDate)
+ds$registerDate <- as.Date(ds$registerDate)
+ds$state <- as.factor(ds$state)
+ds$resolved <- as.factor(ds$resolved)
+
+# translating S (sim) to Y (yes)
+ds$resolved <- recode(ds$resolved, S = 'Y', N = 'N')
+
+# creating a new continuous variable
+ds$elapsedDays <- as.numeric(ds$closingDate - ds$registerDate)
+
+# calculate the frequency table for the state / resolved features
+state_resolved <- ds %>%
+  group_by(state, resolved) %>%
+  summarise (n = n()) %>%
+  mutate(freq = n / sum(n)) %>%
+  subset(resolved == 'Y')
+
+# calculate the median and mean elapsed days per state and resolved features
+state_resolved_elapsedays <- ds %>%
+  group_by(state, resolved) %>%
+  summarise(median = median(elapsedDays), mean = mean(elapsedDays)) %>%
+  subset(resolved == 'Y')
+
+# joining the frequency table of states/resolved with the median elapsed days
+state_resolvedrate_elapsedays <- state_resolved_elapsedays %>%
+  left_join(state_resolved)
+
+# rounding and exporting the data
+state_resolvedrate_elapsedays$mean <- round(state_resolvedrate_elapsedays$mean) / 7
+state_resolvedrate_elapsedays$median <- round(state_resolvedrate_elapsedays$median) / 7
+state_resolvedrate_elapsedays$freq <- round(state_resolvedrate_elapsedays$freq, digits = 3)
+
+
+write.table(state_resolvedrate_elapsedays, file = "data.tsv",
+            quote = FALSE, sep = "\t", row.names=FALSE)
